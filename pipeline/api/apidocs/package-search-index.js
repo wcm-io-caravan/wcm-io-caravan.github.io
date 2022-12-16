f249c2e1f6548a1730e9477edd959e4b27443f73
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2022-12-16 18:11:45 +0000

    Maven site: wcm-io-caravan/caravan-pipeline

---
+++ pipeline/api/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.caravan.pipeline"},{"l":"io.wcm.caravan.pipeline.cache"},{"l":"io.wcm.caravan.pipeline.cache.spi"}]
