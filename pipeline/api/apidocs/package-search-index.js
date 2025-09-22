d35af64e0e0ed104cbfef657d022bc03cd6d7d74
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-09-22 17:24:46 +0000

    Maven site: wcm-io-caravan/caravan-pipeline

--- pipeline/api/apidocs/package-search-index.js
+++ pipeline/api/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.caravan.pipeline"},{"l":"io.wcm.caravan.pipeline.cache"},{"l":"io.wcm.caravan.pipeline.cache.spi"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.wcm.caravan.pipeline"},{"l":"io.wcm.caravan.pipeline.cache"},{"l":"io.wcm.caravan.pipeline.cache.spi"}];updateSearchResults();
